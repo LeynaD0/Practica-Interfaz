1e8e52dc35731230b83b3ff4fe498fbe820521d5
Your Name <you@example.com>
2022-10-20 11:52:06 +0100

    Agregado de escenario

---
+++ Practica-Interfaz/Assets/Scripts/ScreenController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScreenController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject screenGame, screenIntro, player;
+
+    [SerializeField]
+    TMP_InputField inputPlayerName;
+
+    [SerializeField]
+    TextMeshProUGUI name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenGame.SetActive(false);
+        player.SetActive(false);
+        screenIntro.SetActive(true);
+        
+    }
+
+    public void EmpezarJuego()
+    {
+        screenGame.SetActive(true);
+        player.SetActive(true);
+        screenIntro.SetActive(false);
+        Debug.Log(inputPlayerName.text);
+        name.text = inputPlayerName.text;
+    }
+}
